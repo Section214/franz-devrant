34a9598e09699082490798bf0519c60fe4589074
Ghost1227 <dgriffiths@section214.com>
2017-02-24 17:02:25 -0600

    Add help link on my profile

--- webview.js
+++ webview.js
@@ -7,9 +7,19 @@ module.exports = (Franz, options) => {
 	if ($('.addcomment-btn').length) {
 		let rant = $('.body-col2').data('id');
 
+		// Back button
 		$('.rantlist').append('<li class="feed-prev-more"><a class="feed-prev" onclick="window.history.back()" style="cursor: pointer"><span class="icon-back2 icon"></span><span class="feed-prev-more-link">Back</span></a><div class="clearfix"></div></li>');
-		
+
+		// Permalink button
 		$('.share-icons').prepend('<a class="rant-permalink" href="https://www.devrant.io/rants/' + rant + '" target="_blank"><span class="icon-rantsemoticon2 icon"></span><span class="share-text">Permalink</span></a>');
+	}
+	
+	if ($('.profile-bar').length) {
+		let username = $('.username-profile').text();
+		
+		if (username === 'ghost1227') {
+			$('.profile-avatar-container').after('<div class="profile-franz-integration"><a href="https://github.com/Section214/franz-devrant/issues" target="_blank">Hey there! If you\'re seeing this, you\'re using my devRant integration plugin for Franz. Congratulations! If you have questions about this plugin, or have an idea for a new integration, click here and let me know!</a></div>');
+		}
 	}
 	
 	// Add target attribute to app buttons so they open in browser
