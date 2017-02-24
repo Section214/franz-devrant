6304bd6174fa26991c5e133e3227c33123cb4859
Ghost1227 <dgriffiths@section214.com>
2017-02-24 10:26:57 -0600

    Open app links in browser

--- webview.js
+++ webview.js
@@ -1,15 +1,18 @@
 const path = require('path');
 
 module.exports = (Franz, options) => {
-	Franz.injectCSS(path.join(__dirname, 'css', 'franz.css'));
-	
+	// Add back button to individual rants
 	if ($('.addcomment-btn').length) {
 		$('.rantlist').append('<li class="feed-prev-more"><a class="feed-back" onclick="window.history.back()" style="cursor: pointer"><span class="icon-back2 icon"></span><span class="feed-prev-more-link">Back</span></a><div class="clearfix"></div></li>');
 	}
 	
+	// Add target attribute to app buttons so they open in browser
+	$('.app-btns-container a').each( function() {
+		$(this).attr('target', '_blank');
+	});
+	
 	function getMessages() {
 		let unread = '';
-
 
 		if ($('.top-bar-notif.notif-badge').length) {
 			unread = $('.top-bar-notif.notif-badge').text();
@@ -19,4 +22,4 @@ module.exports = (Franz, options) => {
 	}
 
 	Franz.loop(getMessages);
-}
+};
