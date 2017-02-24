b3ddb2bf03433cdfe6a257b217d84d04f3536765
Ghost1227 <dgriffiths@section214.com>
2017-02-24 12:04:44 -0600

    Add permalink

--- webview.js
+++ webview.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
 module.exports = (Franz, options) => {
-	// Add back button to individual rants
+	Franz.injectCSS(path.join(__dirname, 'css', 'franz.css'));
+	
+	// Add back button and permalink to individual rants
 	if ($('.addcomment-btn').length) {
-		$('.rantlist').append('<li class="feed-prev-more"><a class="feed-back" onclick="window.history.back()" style="cursor: pointer"><span class="icon-back2 icon"></span><span class="feed-prev-more-link">Back</span></a><div class="clearfix"></div></li>');
+		let rant = $('.body-col2').data('id');
+
+		$('.rantlist').append('<li class="feed-prev-more"><a class="feed-prev" onclick="window.history.back()" style="cursor: pointer"><span class="icon-back2 icon"></span><span class="feed-prev-more-link">Back</span></a><div class="clearfix"></div></li>');
+		
+		$('.share-icons').prepend('<a class="rant-permalink" href="https://www.devrant.io/rants/' + rant + '" target="_blank"><span class="icon-rantsemoticon2 icon"></span><span class="share-text">Permalink</span></a>');
 	}
 	
 	// Add target attribute to app buttons so they open in browser
@@ -13,7 +19,7 @@ module.exports = (Franz, options) => {
 	
 	// Add help link to devrant menu
 	$('.menu-option-log-out').before('<li><a class="menu-franz" href="https://github.com/section214/franz-devrant/issues" target="_blank"><span class="icon-github2 icon"></span>Franz Integration</a></li>');
-	
+
 	function getMessages() {
 		let unread = '';
 
